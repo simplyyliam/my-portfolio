9643ea0f7a7d9ec05d2ad2e4aa0fac52620b9935
simplyyliam <simplyliam.dev@gmail.com>
2026-02-25 22:20:24 +0200

    ✨ feat(Profiles): implement Profiles.tsx displaying two avatars + add motion library for hover animation

--- src/ui/components/header.tsx
+++ src/ui/components/header.tsx
@@ -1,18 +1,9 @@
-import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { Profiles } from "./profiles";
 
 export const Header = () => {
   return (
-    <div className="flex items-center gap-1">
-      <div className="flex -space-x-4">
-        <Avatar>
-          <AvatarImage src="/favcon.png"/>
-          <AvatarFallback>S.L</AvatarFallback>
-        </Avatar>
-        <Avatar>
-          <AvatarImage src="/pfp.png"/>
-          <AvatarFallback>S.L</AvatarFallback>
-        </Avatar>
-      </div>
+    <div className="flex items-center gap-4">
+      <Profiles />
       Hey, I&apos;m liam! I&apos;m a Developer
     </div>
   );
--- src/ui/index.ts
+++ src/ui/index.ts
@@ -1,3 +1,4 @@
 export { ThemeProvider } from "./components/theme-provider"
 export { Wrapper } from "./components/wrapper"
 export { Header } from "./components/header"
+export { Profiles } from "./components/profiles"
