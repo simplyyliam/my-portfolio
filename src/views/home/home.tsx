bf37ef4c51b56c1f575853886528e58abac725f1
simplyyliam <simplyliam.dev@gmail.com>
2026-03-23 13:05:06 +0200

    feat(home): improve responsive design

--- src/views/home/home.tsx
+++ src/views/home/home.tsx
@@ -2,16 +2,16 @@ import { Builds, Header, Wrapper } from "../../ui";
 
 export default function Home() {
   return (
-    <div className="flex w-full gap-5 justify-center">
-      <div className="relative w-60 max-w-md aspect-square">
+    <div className="flex w-full flex-col items-center justify-center gap-6 px-4 sm:flex-row sm:items-start sm:gap-5 sm:px-10 ">
+      <div className="relative aspect-square w-full max-w-60 sm:w-60 sm:max-w-md">
         <iframe
           src="https://commit-sphere.vercel.app/embed?user=simplyyliam"
           title="Commit Sphere for simplyyliam"
-          className="absolute inset-0 w-60 h-60 border-[0.75px] rounded"
+          className="absolute inset-0 h-full w-full rounded border-[0.75px]"
         />
       </div>
       <Wrapper>
-        <div className="flex flex-col w-full gap-2.5 sm:h-100 h-auto">
+        <div className="flex h-auto w-full flex-col gap-2.5 sm:h-100">
           <Header />
           <p>
             Design engineer in the making. Focused on interaction, motion, and
